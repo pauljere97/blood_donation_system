d04d47aaaf90d47a4b18baebb8304a2030cf0b5e
Paul Jere <pauljere97@gmail.com>
2022-10-05 12:14:08 +0200

    learn more

--- src/components/LearnText.jsx
+++ src/components/LearnText.jsx
@@ -12,17 +12,13 @@ const LearnText = () => {
                         <h1 style={{color:'#EB4D4A'}}>What is Blood Donation</h1>
                         <p>Commodo labore ipsum eu veniam reprehenderit nostrud magna pariatur ex nostrud. Deserunt labore excepteur consequat deserunt ea ea reprehenderit amet. Et commodo ex voluptate eiusmod duis officia consequat sint ex minim.</p>
                     </div>
-                    <div className="register_pad">
-                        
-                    </div>
+                    <img style={{width:'300px'}} src={require('../assets/bloodicon.png')} alt="" />
                 </div>
                 <div>
+                    <img style={{width:'300px', margin:'0 50px'}} src={require('../assets/bloodicon.png')} alt="" />
                     <div className="text_side">
                         <h1 style={{color:'#EB4D4A'}}>Who can donate</h1>
                         <p>Commodo labore ipsum eu veniam reprehenderit nostrud magna pariatur ex nostrud. Deserunt labore excepteur consequat deserunt ea ea reprehenderit amet. Et commodo ex voluptate eiusmod duis officia consequat sint ex minim.</p>
-                    </div>
-                    <div className="register_pad">
-                        
                     </div>
                 </div>
                 <div>
@@ -30,18 +26,17 @@ const LearnText = () => {
                         <h1 style={{color:'#EB4D4A'}}>Where does blood go</h1>
                         <p>Commodo labore ipsum eu veniam reprehenderit nostrud magna pariatur ex nostrud. Deserunt labore excepteur consequat deserunt ea ea reprehenderit amet. Et commodo ex voluptate eiusmod duis officia consequat sint ex minim.</p>
                     </div>
-                    <div className="register_pad">
-                        
-                    </div>
+                    <img style={{width:'300px'}} src={require('../assets/bloodicon.png')} alt="" />
                 </div>
                 <div>
+                    <img style={{width:'300px', margin:'0 50px'}} src={require('../assets/bloodicon.png')} alt="" />
                     <div className="text_side">
                         <h1 style={{color:'#EB4D4A'}}>What are the risks of donating blood</h1>
                         <p>Commodo labore ipsum eu veniam reprehenderit nostrud magna pariatur ex nostrud. Deserunt labore excepteur consequat deserunt ea ea reprehenderit amet. Et commodo ex voluptate eiusmod duis officia consequat sint ex minim.</p>
                     </div>
-                    <div className="register_pad">
+                    {/* <div className="register_pad">
                         
-                    </div>
+                    </div> */}
                 </div>
             </section>
     )
